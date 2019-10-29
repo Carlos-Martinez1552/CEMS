ad30bc6cd0e650997667f22a9d3dd9b7395598ce
Carlos Eduardo Martinez Salgado <54385540+Carlos-Martinez1552@users.noreply.github.com>
2019-10-29 18:02:42 -0500

    Add files via upload

---
+++ Small_Triangules_Largues_Triangles.c
@@ -0,0 +1,17 @@
+
+int heron(triangle *t){
+    int twop=t->a+t->b+t->c;
+    return twop*(twop-2*t->a)*(twop-2*t->b)*(twop-2*t->c);
+}
+
+int heron_comp( const void *a, const void *b){
+    return heron((triangle*)a) - heron((triangle*)b);
+
+}
+
+void sort_by_area(triangle* tr, int n) {
+    /**
+    * Sort an array a of the length n
+    */
+    qsort(tr, n, sizeof(*tr), heron_comp);
+}
